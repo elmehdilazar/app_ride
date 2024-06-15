14254d8dfd95863ec1358971ef99cc3855914fe7
elmehdilazar <midomidomido01@gmail.Com>
2024-06-15 11:00:30 +0100

    add

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,8 +21,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    
 });
 Route::apiResource('cars', CarsController::class);
 Route::apiResource('garages', GaragesController::class);
